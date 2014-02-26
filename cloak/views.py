c27d2b12b43bc58261ae69ef90036beb462221b1
Matt Johnson <mdj2@pdx.edu>
2014-02-26 12:58:55 -0800

    Fix some bugs in code and documentation; Still haven't tested this yet...

--- cloak/views.py
+++ cloak/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.core.signing import TimestampSigner, BadSignature, SignatureExpired
 from django.contrib.auth import get_user_model, login as django_login, REDIRECT_FIELD_NAME
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
 from django.shortcuts import get_object_or_404
 from django.utils.http import is_safe_url
 from . import MAX_AGE_OF_SIGNATURE_IN_SECONDS, SESSION_USER_KEY, SESSION_REDIRECT_KEY
@@ -12,7 +13,7 @@ from . import MAX_AGE_OF_SIGNATURE_IN_SECONDS, SESSION_USER_KEY, SESSION_REDIREC
 def login(request, signature):
     """
     Automatically logs in a user based on a signed PK of a user object. The
-    signature should be generated with the `cloak` management command.
+    signature should be generated with the `login` management command.
 
     The signature will only work for 60 seconds.
     """
@@ -29,7 +30,8 @@ def login(request, signature):
 
     return HttpResponseRedirect(settings.LOGIN_REDIRECT_URL)
 
-@login_required
+@login_requied
+@require_POST
 def cloak(request, pk=None):
     """
     Masquerade as a particular user and redirect based on the
@@ -38,9 +40,6 @@ def cloak(request, pk=None):
     Callers can either pass the pk of the user in the URL itself, or as a POST
     param.
     """
-    if request.method != 'POST':
-        return HttpResponse("I only respond to POSTs")
-
     pk = request.POST.get('pk', pk)
     if pk is None:
         return HttpResponse("You need to pass a pk POST parameter, or include it in the URL")
@@ -56,23 +55,32 @@ def cloak(request, pk=None):
         except AttributeError as e:
             pass
 
+    if can_cloak:
+        return HttpResponseForbidden("You are not allowed to cloak as this user")
+
     request.session[SESSION_USER_KEY] = user.pk
+    # save the referer information so when uncloaking, we can redirect the user
+    # back to where they were
     request.session[SESSION_REDIRECT_KEY] = request.META.get("HTTP_REFERER", settings.LOGIN_REDIRECT_URL)
-    return HttpResponseRedirect(request.GET(REDIRECT_FIELD_NAME, settings.LOGIN_REDIRECT_URL))
+
+    # redirect the cloaked user to the URL specified in the "next" parameter,
+    # or to the default redirect URL
+    return HttpResponseRedirect(request.POST.get(REDIRECT_FIELD_NAME, settings.LOGIN_REDIRECT_URL))
 
 # no perms neccessary here
+@require_POST
 def uncloak(request):
-    """Undo a masquerade session"""
-    if request.method != 'POST':
-        return HttpResponse("I only respond to POSTs")
-
+    """
+    Undo a masquerade session and redirect the user back to where they started
+    cloaking from (or where ever the "next" POST parameter points)
+    """
     try:
         del request.session[SESSION_USER_KEY]
     except KeyError:
         pass # who cares
 
     # figure out where to redirect
-    next = request.GET.get(REDIRECT_FIELD_NAME, request.session.get(SESSION_REDIRECT_KEY))
+    next = request.POST.get(REDIRECT_FIELD_NAME, request.session.get(SESSION_REDIRECT_KEY))
     if next is not None and is_safe_url(next, request.request.get_host()):
         return HttpResponseRedirect(next)
     return HttpResponseRedirect(settings.LOGIN_REDIRECT_URL)
